b03e17cb456c3ced20a9d9d3c82c5d8140205f5e
wwlhwt <32184004+wwlhwt@users.noreply.github.com>
2023-02-15 17:10:52 +0800

    Create time5.c

---
+++ time5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sigalrm_fn(int sig)
+{
+    printf("alarm!\n");
+    alarm(2);
+    return;
+}
+
+int main(void)
+{
+    signal(SIGALRM, sigalrm_fn);
+    alarm(2);
+
+    while(1) pause();
+}
+
+/*
+alarm!
+alarm!
+alarm!
+gcc test5.c -o test5
+*/
